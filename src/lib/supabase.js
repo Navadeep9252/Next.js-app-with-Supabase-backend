12724374551750a3c3f461b8ffbfb48f2ed8ef78
Navadeep9252 <karkondareddy1@gmail.com>
2025-08-29 18:31:39 +0200

    update message

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Get your Supabase credentials from environment variables
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = "https://hswdiqfyduagwgunhazh.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhzd2RpcWZ5ZHVhZ3dndW5oYXpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYyMjAxNzUsImV4cCI6MjA3MTc5NjE3NX0.u9X030NnT0dYWmrs6uq8UhEj4SMqVlQylTW9tlcnaJA"
 
 // Initialize client
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
